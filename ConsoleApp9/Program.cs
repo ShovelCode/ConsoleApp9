b143862112b7ced55f2b7eb729c93dc1d6a775e4
ShovelCode <vde435@gmail.com>
2020-12-03 14:08:57 -0600

    new function

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+
 namespace ConsoleApp9
 {
     class Program
@@ -27,6 +28,23 @@ namespace ConsoleApp9
                 Console.WriteLine($"   {city}");
             }
 
+            mapSample();
+
+        }
+
+        static void mapSample()
+        {
+            var keywords = new Dictionary<string, string>();
+            keywords.Add("int", "32-bit integer data type");
+            keywords.Add("long", "64-bit integer data type");
+            keywords.Add("float", "Single precision floating point number");
+
+            Console.WriteLine("keywords and their definitions.");
+
+            foreach (KeyValuePair<string, string> item in keywords)
+            {
+                Console.WriteLine($"  {item.Key}: {item.Value}");
+            }
         }
     }
 }
