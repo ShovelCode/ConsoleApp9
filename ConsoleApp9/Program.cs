383edba5f961209f47a71712f28d172cbef48973
ShovelCode <vde435@gmail.com>
2020-12-03 00:33:23 -0600

    update

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -7,6 +7,15 @@ namespace ConsoleApp9
         static void Main(string[] args)
         {
             Console.WriteLine("Test String");
+
+            string cities = "Paris,Berlin,Madrid,New York,Comma Town";
+
+            string[] citiesArray = cities.Split(',');
+
+            foreach( string item in citiesArray)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
