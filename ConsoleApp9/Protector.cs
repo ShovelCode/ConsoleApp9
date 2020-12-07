9dbc0daa031ce650fa692b26e0bb9dad6575bf03
ShovelCode <vde435@gmail.com>
2020-12-07 00:15:37 -0600

    adding a new class

---
+++ ConsoleApp9/Protector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Xml.Linq;
+using static System.Convert;
+
+namespace ConsoleApp9
+{
+    class Protector
+    {
+        private static readonly byte[] salt = Encoding.Unicode.GetBytes("7Bananas");
+    }
+}
