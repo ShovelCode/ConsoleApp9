516649bcd053c53366708b294ad4feead9e614a5
ShovelCode <vde435@gmail.com>
2020-12-10 16:51:57 -0600

    update

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -33,7 +33,7 @@ namespace ConsoleApp9
             //mapSample();
             //analyze_Url();
             //makePing();
-            testControl();
+            dataTypeSample();
 
         }
 
@@ -83,6 +83,48 @@ namespace ConsoleApp9
         static void testControl()
         {
             Console.WriteLine("In textControl method");
+        }
+
+        static void dataTypeSample()
+        {
+            string verbatimSample = @"\\\n~`.'";
+            string normalString = "Greetings";
+            double piApproximation = 3.14;
+            int bigNum = 70_000_000;
+            long longerInt = 10000000;
+            decimal price = 9.99M;
+            char letter = 's';
+            string deseret = "𐐯"; //deseret sample
+            bool greenLight = false;
+            //string password = string.Empty;
+
+            string[] names = new string[4];
+
+            names[0] = "Ert";
+            names[1] = "Jill";
+            names[2] = "Jack";
+            names[3] = "Three";
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            Console.WriteLine("Hello Again");
+            Console.WriteLine(verbatimSample);
+            Console.WriteLine(sizeof(int));
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(normalString);
+            Console.WriteLine(piApproximation);
+            Console.WriteLine(letter);
+            Console.WriteLine(bigNum);
+            Console.WriteLine(greenLight);
+            Console.WriteLine(deseret);
+            Console.WriteLine(price);
+            Console.WriteLine(longerInt);
+            //TimesTable(7);
+            //Console.WriteLine(Factorial(4));
+            Console.WriteLine();
         }
     }
 }
